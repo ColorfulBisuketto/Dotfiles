b0d316f40da3c9a61dc65eb9800f158ff9975def
Colorful Bisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>
2025-05-13 23:58:59 +0200

    Added: neovim, basic scripts.

---
+++ copy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+configured_packages=("cava" "fastfetch" "hypr" "kitty" "nvim" "qt5ct" "qt6ct" "rofi" "starship.toml" "swaync" "uwsm" "waybar" "yazi")
+
+for pack in ${configured_packages[@]}; do
+  rm -rf "$SCRIPT_DIR/.config/$pack/"
+  cp -r "$XDG_CONFIG_HOME/$pack" "$SCRIPT_DIR/.config/"
+done
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+configured_packages=("cava" "fastfetch" "hypr" "kitty" "nvim" "qt5ct" "qt6ct" "rofi" "starship.toml" "swaync" "uwsm" "waybar" "yazi")
+
+[ ! -d "$XDG_CONFIG_HOME.bak/" ] && mkdir "$XDG_CONFIG_HOME.bak/"
+
+for pack in ${configured_packages[@]}; do
+  mv -iv "$XDG_CONFIG_HOME/$pack/" "$XDG_CONFIG_HOME.bak/$pack/"
+  cp -r "$SCRIPT_DIR/.config/$pack/" "$XDG_CONFIG_HOME/"
+done
