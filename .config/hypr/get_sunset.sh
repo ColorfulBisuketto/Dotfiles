65c738314440dafae90e77454fe942742ecbb018
Colorful Bisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>
2025-05-29 11:25:49 +0200

    Formatted waybar config.

--- .config/hypr/get_sunset.sh
+++ .config/hypr/get_sunset.sh
@@ -3,4 +3,4 @@
 # Sunset time in epoch timestamp
 SUNSET=$(curl -s 'https://api.sunrisesunset.io/json?lat=49.1625434&lng=8.0349548' | jq '.results.dusk' | xargs date "+%s" -d)
 
-echo $(date -d "@$(($SUNSET - 3600))" "+%H:%M:%S")
+echo $(date -d "@$(($SUNSET - 2700))" "+%H:%M:%S")
