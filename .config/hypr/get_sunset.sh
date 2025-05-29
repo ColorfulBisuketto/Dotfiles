fb4aaf2a997070b243e385f43dccf86b771886f3
Colorful Bisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>
2025-05-29 12:30:54 +0200

    Clean up location reading

--- .config/hypr/get_sunset.sh
+++ .config/hypr/get_sunset.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 # Import location
+if [[ ! -f ~/.config/hypr/sunset_location ]]; then
+  echo "lat=" >~/.config/hypr/sunset_location
+  echo "lng=" >>~/.config/hypr/sunset_location
+fi
 source ~/.config/hypr/sunset_location
+if [[ -z $lat || -z $lng ]]; then
+  echo "Location is not set."
+  echo "please set the latitude and longitude for your location."
+  exit 1
+fi
 
 # Sunset time in epoch timestamp
 SUNSET=$(curl -s "https://api.sunrisesunset.io/json?lat=$lat&lng=$lng" | jq ".results.dusk" | xargs date "+%s" -d)
