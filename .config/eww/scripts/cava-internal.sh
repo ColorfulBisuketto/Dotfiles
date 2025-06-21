c56eb0ac4663fefcf1da069c04b3b43e471e11f3
Colorful Bisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>
2025-06-21 21:32:26 +0200

    Adjusted the Eww bar and config structure.

--- .config/eww/scripts/cava-internal.sh
+++ .config/eww/scripts/cava-internal.sh
@@ -20,4 +20,4 @@ if [ -p $pipe ]; then
 fi
 mkfifo $pipe
 
-printf "[general]\nframerate=60\nbars = 17\nsleep_timer=3\n[output]\nmethod = raw\nraw_target = /dev/stdout\ndata_format = ascii\nascii_max_range = 7\n" | cava -p /dev/stdin | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g; '
+printf "[general]\nframerate=60\nbars = 37\nsleep_timer=3\n[output]\nmethod = raw\nraw_target = /dev/stdout\ndata_format = ascii\nascii_max_range = 7\n" | cava -p /dev/stdin | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g; '
