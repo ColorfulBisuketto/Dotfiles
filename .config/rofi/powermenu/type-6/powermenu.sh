f3e20867a70ecfc53f17a17d610c97303923a615
Colorful Bisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>
2025-07-30 00:04:14 +0200

    Restructured where scripts are and how hyprsunset.
      Scripts are now in the config folder for the programs that use them.
    
      hyprsunset is now using its actual config file instead of some random
      scripts.

--- .config/rofi/powermenu/type-6/powermenu.sh
+++ .config/rofi/powermenu/type-6/powermenu.sh
@@ -18,20 +18,20 @@ lastlogin="$(last $USER | head -n1 | tr -s ' ' | cut -d' ' -f5,6,7)"
 uptime="$(uptime -p | sed -e 's/up //g')"
 
 # Options
-hibernate=''
-shutdown=''
-reboot=''
-lock=''
-suspend=''
-logout=''
-yes=''
-no=''
+hibernate='󰏦'
+shutdown='⏻'
+reboot=''
+lock='󰌾'
+suspend='󰤄'
+logout='󰗽'
+yes=''
+no=''
 
 # Rofi CMD
 rofi_cmd() {
   rofi -dmenu \
-    -p " $USER@$HOSTNAME" \
-    -mesg " Uptime: $uptime" \
+    -p " $USER@$HOSTNAME" \
+    -mesg " Uptime: $uptime" \
     -theme ${dir}/${theme}.rasi
 }
 
--- copy.sh
+++ copy.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-configured_packages=("cava" "fastfetch" "hypr" "kitty" "nvim" "qt5ct" "qt6ct" "rofi" "starship.toml" "swaync" "uwsm" "yazi" "custom-scripts" "eww" "btop" "qBittorrent" "starship.toml")
+configured_packages=("cava" "fastfetch" "hypr" "kitty" "nvim" "qt5ct" "qt6ct" "rofi" "swaync" "uwsm" "yazi" "eww" "btop")
 
 for pack in ${configured_packages[@]}; do
   rm -rf "$SCRIPT_DIR/.config/$pack/"
